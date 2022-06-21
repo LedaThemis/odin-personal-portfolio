c96f520959e3ceaa65f0bf51151b4c979b33b540
Leda <ledathemis@protonmail.com>
2022-06-21 17:51:24 +0000

    render Projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import AboutMe from "./components/AboutMe";
+import styled from 'styled-components';
+
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
 
 const App = () => {
-  return <main style={{display: 'flex', justifyContent: 'center'}}>
-    <AboutMe />
-  </main>;
-}
+  return (
+    <StyledMainContainer>
+      <AboutMe />
+      <Projects />
+    </StyledMainContainer>
+  );
+};
+
+const StyledMainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 100px;
+
+  padding: 64px;
+  box-sizing: border-box;
+`
 
 export default App;
