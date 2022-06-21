8db50b2a7d8519adcb879fd8939d0ee7e2f98c3e
Leda <ledathemis@protonmail.com>
2022-06-21 20:43:02 +0000

    add Contact component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <StyledMainContainer>
       <AboutMe />
       <Projects />
+      <Contact />
     </StyledMainContainer>
   );
 };
@@ -18,8 +20,11 @@ const StyledMainContainer = styled.main`
   align-items: center;
   gap: 100px;
 
-  padding: 64px;
-  box-sizing: border-box;
-`
+  & > * {
+    padding-left: 64px;
+    padding-right: 64px;
+    box-sizing: border-box;
+  }
+`;
 
 export default App;
